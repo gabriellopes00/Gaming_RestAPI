d17d38f333d62a4096f9d9c7685729a84909f7c4
Gabriel Lopes <gabrieluislopes00@gmail.com>
2020-10-26 16:14:48 -0300

    Adapting db connection

--- database/connection.js
+++ database/connection.js
@@ -1,9 +1,12 @@
 //Imports
   import Sequelize from 'sequelize';
 
+  import dbInformation from '../node_modules/information/index';
+  //Here i suggest you create an array with your db information (db name, user name, user password and your host), in a folder inside 'node_modules' and use ".gitignore", to omit this information in your github repository;
+
 // db Connection  
-const connection = new Sequelize('games', 'root', 'root',{
-  host: 'localhost', 
+const connection = new Sequelize(dbInformation[0], dbInformation[1], dbInformation[2],{
+  host: dbInformation[3], 
   dialect: 'mysql'
 })
 
